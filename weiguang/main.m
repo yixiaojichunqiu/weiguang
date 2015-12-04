e01e787f212db3ff35be64136b73c7f86aa68e64
tiankey <tiankey@tarenadeimac-35.local>
2015-12-03 20:28:01 -0800

    Initial Commit

---
+++ weiguang/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  weiguang
+//
+//  Created by tiankey on 15/12/3.
+//  Copyright © 2015年 tiankey. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
